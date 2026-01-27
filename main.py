7b9f4c3f26ca32f8d38606e09d4622541c20cbf2
Krishna Biswal <krshnabiswal619@gmail.com>
2026-01-27 08:59:13 +0530

    Added utils

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
+import getpass
+
+import typer
+from rich import print
+
+from utils import generate_words
+
+username = getpass.getuser()
+app = typer.Typer()
+
+
+@app.callback(invoke_without_command=True)
 def main():
-    print("Hyper Type")
+    print(f"""Welcome to HyperType!, {username}
+    Practice daily • Improve speed • Boost accuracy
+
+    Features:
+    - 15s / 30s / 60s typing modes
+    - Real-time feedback
+    - WPM + accuracy tracking
+    - Daily typing exercises
+
+    Start typing. Start improving.
+""")
+
+
+@app.command()
+def start(mode: int):
+    words = generate_words(mode)
+    pass
+
+
+if __name__ == "__main__":
+    app()
---
+++ utils.py
@@ -0,0 +1,39 @@
+import os
+import random
+
+
+def generate_words(word_count: int):
+    """
+    Generate a string of random words separated by spaces.
+    Args:
+        word_count (int): The number of words to generate.
+    Returns:
+        str: A string of random words.
+    """
+    random_words = [
+        "python",
+        "developer",
+        "terminal",
+        "keyboard",
+        "docker",
+        "react",
+        "cloud",
+        "system",
+        "performance",
+        "latency",
+        "database",
+        "backend",
+        "frontend",
+        "typescript",
+        "graphql",
+        "microservice",
+        "distributed",
+    ]
+    return " ".join(random.choices(random_words, k=word_count))
+
+
+def clear_terminal():
+    """
+    Clear the terminal screen.
+    """
+    os.system("cls" if os.name == "nt" else "clear")
